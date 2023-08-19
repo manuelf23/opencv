62564efe5e6e407504c9143c1ed4f5e02b830b09
Manuel Garcia <manuelf.garcia23@gmail.com>
2023-08-19 09:00:59 -0500

    fix(samples): Update code for lates matplotlib in 3d projection

--- samples/python/camera_calibration_show_extrinsics.py
+++ samples/python/camera_calibration_show_extrinsics.py
@@ -197,10 +197,8 @@ def main():
     extrinsics = fs.getNode('extrinsic_parameters').mat()
 
     import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D  # pylint: disable=unused-variable
 
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+    ax = plt.figure().add_subplot(projection='3d')
     ax.set_aspect("auto")
 
     cam_width = args.cam_width
